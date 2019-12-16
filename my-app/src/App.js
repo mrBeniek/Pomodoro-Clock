bc3d90b05938e7804fa0102ab51ef5d18a90723e
mrBeniek <b3niek@gmail.com>
2019-12-16 16:22:46 +0100

    added items

---
+++ my-app/src/App.js
@@ -0,0 +1,308 @@
+import React from 'react';
+import './App.scss';
+
+let secs = 1;
+let mins = 25;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: "25:00",
+      break: 5,
+      session: 25,
+      sessionStatus: "session",
+      status: "stop",
+    };
+    this.handleTimer = this.handleTimer.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.breakDecrement = this.breakDecrement.bind(this);
+    this.breakIncrement = this.breakIncrement.bind(this);
+    this.sessionDecrement = this.sessionDecrement.bind(this);
+    this.sessionIncrement = this.sessionIncrement.bind(this);
+    
+    
+  }
+  
+  
+ handleTimer() {
+   
+    this.setState( state => {
+      if (state.status === "stop") {
+        return {status: "play"};
+      } else {
+        return {status: "stop"};
+      }        
+    })
+   
+
+     let countdown =  setInterval( () => {
+        if (this.state.status == "stop") {
+           clearInterval(countdown);
+           return
+        }
+       
+        if (!secs && !mins) {
+          document.getElementById("beep").play();
+          if (this.state.sessionStatus == "session") {
+            mins = this.state.break;
+            secs = 1;
+            this.setState({
+              sessionStatus: "break"           
+            })          
+          } 
+          else {
+              mins = this.state.session;
+              secs = 1;
+              this.setState({
+                sessionStatus: "session"
+              })         
+          }  
+        } 
+       
+        if (secs != 0) {secs = secs - 1}
+         else {
+          mins = mins - 1;
+          secs = 59;
+         }
+       
+       if (mins < 10 && secs < 10) {
+         this.setState({
+          timer: "0" + mins + ":" + "0" + secs
+        });
+       }
+       else if (mins < 10) {
+         this.setState({
+          timer: "0" + mins + ":" + secs
+        });
+       } 
+       else if (secs < 10) {
+         this.setState({
+          timer: mins + ":" + "0" + secs
+        });
+       } else
+        this.setState({
+          timer: mins + ":" + secs
+        });
+      }, 1000);
+    
+  }
+  
+  handleStop() {
+    this.setState( state => {
+      if (state.status === "stop") {
+        return {status: "play"};
+      } else {
+        return {status: "stop"};
+      }        
+    })
+  }
+  
+  handleReset() {
+    document.getElementById("beep").pause();
+    document.getElementById("beep").currentTime = 0;
+    this.setState({
+      timer: "25:00",
+      break: 5,
+      session: 25,
+      sessionStatus: "session",
+      status: "stop",
+    })
+      secs = 0;
+      mins = 25;
+  }
+  
+  breakDecrement() {
+    if (this.state.break > 1 && this.state.status == "stop" )
+      this.setState({
+        break: this.state.break - 1
+      })
+  }
+  
+  breakIncrement() {
+    if (this.state.break < 60 && this.state.status == "stop")
+      this.setState({
+        break: this.state.break + 1
+      })
+  }
+  
+  sessionDecrement() {
+    if (this.state.session > 1 && this.state.status == "stop") {
+      if (this.state.session < 11) {
+        this.setState ({
+         session: this.state.session - 1,
+         timer: "0" + (this.state.session - 1) + ":00" 
+        })
+      } else {
+      this.setState({
+        session: this.state.session - 1,
+        timer: (this.state.session - 1) + ":00"
+      })};
+        mins -= 1;
+    }
+       
+    
+      
+  }
+  
+  sessionIncrement() {
+    if (this.state.session < 60 && this.state.status == "stop") {
+      if (this.state.session < 9) {
+        this.setState ({
+         session: this.state.session + 1,
+         timer: "0" + (this.state.session + 1) + ":00" 
+        })
+       } else
+         {
+      this.setState({
+        session: this.state.session + 1,
+        
+        timer: (this.state.session + 1) + ":00"
+      }) }
+        mins += 1;
+    }
+  }
+  
+  
+  render() {
+     
+       
+    return (
+      <div id="main">
+        <div id="main-container">
+          
+            <div id="left-container">
+              
+              <Display timer={this.state.timer} sessionStatus={this.state.sessionStatus} />
+              <Controls handleTimer={this.handleTimer} handleReset={this.handleReset} />
+          </div>
+          <div id="right-container">
+            <BreakLength breakLength={this.state.break} breakDecrement={this.breakDecrement} breakIncrement={this.breakIncrement} />
+            
+            <SessionLength sessionLength={this.state.session} sessionDecrement={this.sessionDecrement} sessionIncrement={this.sessionIncrement} />
+            </div>
+            
+          </div>
+        
+        <audio id="beep" src="http://soundbible.com/grab.php?id=1252&type=mp3"></audio>
+      </div>
+    )
+  }
+}
+
+class Display extends React.Component {
+  
+  render() {
+   
+    const sess = <div id="timer-label">CURRENT SESSION</div>;
+    const brk = <div id="timer-label">BREAK TIME</div>;       
+        
+    return (
+      <div id="display-container">
+      <div id="title">POMODORO CLOCK</div>
+    <div id="timer-container">
+          
+      <div id="time-left">
+          {this.props.timer}
+        
+        </div>   
+              
+      {this.props.sessionStatus == "session" ? null : brk}
+        
+        
+        </div>
+    </div>
+    )
+  }
+} 
+
+class Controls extends React.Component {
+  
+  render() {
+       
+  
+     
+    return (
+    
+        <div className="controls-container">
+          
+          <div id="start-stop" className="controls-button" onClick={this.props.handleTimer}>
+            PLAY
+          </div>
+          
+          <div id="reset" className="controls-button" onClick={this.props.handleReset}>
+            RESET
+          </div>
+          
+          
+          
+        </div>
+    
+    )
+  }
+} 
+
+
+class BreakLength extends React.Component {
+  
+  render() {
+    return (
+    <div id="break-container" >
+        <div className="center-container">
+                    
+          <div id="break-label" className="label">
+            BREAK LENGTH
+          </div>      
+                
+        </div>
+        
+        <div id="break-controls" className="center-container">
+           <div id="break-decrement" className="button-minus" onClick={this.props.breakDecrement}>
+            -
+          </div>
+          <div className="timer-increment">
+              {this.props.breakLength}
+          </div>
+          <div id="break-increment" className="button-plus" onClick={this.props.breakIncrement}>
+             +
+          </div>
+             </div>
+    </div>
+    )
+  }
+} 
+
+
+
+class SessionLength extends React.Component {
+  
+  render() {
+    return (
+    <div id="session-container">
+        <div className="center-container">
+              
+          <div id="session-label" className="label">
+            SESSION LENGTH
+          </div>
+         
+        </div>
+        
+        <div className="center-container">
+          <div className="button-minus" onClick={this.props.sessionDecrement}>
+            -
+          </div>
+          <div id="session-timer" className="timer-increment">
+              {this.props.sessionLength}
+             </div>
+        <div className="button-plus" onClick={this.props.sessionIncrement}>
+           +
+          </div>
+        </div>
+    </div>
+    )
+  }
+} 
+
+
+
+export default App;
