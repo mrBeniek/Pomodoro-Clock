ddac4ca3d87b05270c3b3313e217c54ad8e7481e
mrBeniek <b3niek@gmail.com>
2019-12-17 17:00:52 +0100

    added component SessionLength

---
+++ my-app/src/Components/SessionLength.js
@@ -0,0 +1,34 @@
+
+
+class SessionLength extends React.Component {
+  
+    render() {
+      return (
+      <div id="session-container">
+          <div className="center-container">
+                
+            <div id="session-label" className="label">
+              SESSION LENGTH
+            </div>
+           
+          </div>
+          
+          <div className="center-container">
+            <div className="button-minus" onClick={this.props.sessionDecrement}>
+              -
+            </div>
+            <div id="session-timer" className="timer-increment">
+                {this.props.sessionLength}
+               </div>
+          <div className="button-plus" onClick={this.props.sessionIncrement}>
+             +
+            </div>
+          </div>
+      </div>
+      )
+    }
+  } 
+  
+  
+  
+  export default SessionLength;
